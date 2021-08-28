a2a9c50cf9449ab00fc824c4469962570dbefb84
Sylvio Maggessi <sylviodev@gmail.com>
2021-08-28 11:56:35 -0300

    first commit

---
+++ lib/app_widget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:playflow/modules/barcode_scanner/barcode_scanner_page.dart';
+import 'package:playflow/modules/home/home_page.dart';
+import 'package:playflow/modules/insert_boleto/insert_boleto_page.dart';
+import 'package:playflow/modules/login/login_page.dart';
+import 'package:playflow/modules/splash/splash_page.dart';
+import 'package:playflow/shared/themes/app_colors.dart';
+
+class AppWidget extends StatelessWidget {
+  AppWidget() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pay Flow',
+      theme: ThemeData(
+          primaryColor: AppColors.primary, primarySwatch: Colors.orange),
+      initialRoute: "/splash",
+      routes: {
+        "/splash": (context) => SplashPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+        "/barcode_scanner": (context) => BarcodeScannerPage(),
+        "/insert_boleto": (context) => InsertBoletoPage(),
+      },
+    );
+  }
+}
